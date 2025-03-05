cdbd7cc6d94db3fa2c76d34a09d581aa2dfc22c7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-05 20:54:37 +0630

    -

--- DotNet8.Architecture.DbService/AppDbContextModels/JobQueue.cs
+++ DotNet8.Architecture.DbService/AppDbContextModels/JobQueue.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.Architecture.DbService.AppDbContextModels;
+﻿namespace DotNet8.Architecture.DbService.AppDbContextModels;
 
 public partial class JobQueue
 {
