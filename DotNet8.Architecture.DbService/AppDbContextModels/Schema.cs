473f7a83e7a3e1ea68af52a9a5203fa2454c7a79
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-05 13:51:22 +0630

    -

---
+++ DotNet8.Architecture.DbService/AppDbContextModels/Schema.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.Architecture.DbService.AppDbContextModels;
+
+public partial class Schema
+{
+    public int Version { get; set; }
+}
