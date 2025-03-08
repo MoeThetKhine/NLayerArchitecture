8654094e27c19d109bb2c75bfdd369d942ce344f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 09:54:06 +0630

    -

--- DotNet8.Architecture.DataAccess/Features/Blog/DA_Blog.cs
+++ DotNet8.Architecture.DataAccess/Features/Blog/DA_Blog.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architecture.DataAccess.Features.Blog
+﻿namespace DotNet8.Architecture.DataAccess.Features.Blog
 {
 	public class DA_Blog
 	{
