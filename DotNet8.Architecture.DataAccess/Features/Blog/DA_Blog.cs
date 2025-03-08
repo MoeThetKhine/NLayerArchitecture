edc811a75611d807562abc317bdca417650ee7f2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 10:06:30 +0630

    -

--- DotNet8.Architecture.DataAccess/Features/Blog/DA_Blog.cs
+++ DotNet8.Architecture.DataAccess/Features/Blog/DA_Blog.cs
@@ -1,5 +1,15 @@
-﻿namespace DotNet8.Architecture.DataAccess.Features.Blog;
+﻿using DotNet8.Architecture.DbService.AppDbContextModels;
+
+namespace DotNet8.Architecture.DataAccess.Features.Blog;
 
 public class DA_Blog
 {
+	private readonly AppDbContext _context;
+
+	public DA_Blog(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<Result<>>
 }
