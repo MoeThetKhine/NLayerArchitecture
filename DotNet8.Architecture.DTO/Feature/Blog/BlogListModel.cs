cfe1b8733ceacfa4a9346e07b6cd0323b873922d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 10:17:57 +0630

    -

--- DotNet8.Architecture.DTO/Feature/Blog/BlogListModel.cs
+++ DotNet8.Architecture.DTO/Feature/Blog/BlogListModel.cs
@@ -7,9 +7,13 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architecture.DTO.Feature.Blog
 {
+	#region BlogListModel
+
 	public class BlogListModel
 	{
 		public IEnumerable<BlogModel> DataLst { get; set; }
 		public PageSettingModel PageSetting { get; set; }
 	}
+
+	#endregion
 }
