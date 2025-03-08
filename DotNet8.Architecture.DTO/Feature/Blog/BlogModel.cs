c8bf09544eb8459c26923a5870978edc36e7ccbb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 10:06:17 +0630

    Feat : Add Blog Model

--- DotNet8.Architecture.DTO/Feature/Blog/BlogModel.cs
+++ DotNet8.Architecture.DTO/Feature/Blog/BlogModel.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architecture.DTO.Feature.Blog
 {
-	internal class BlogModel
+	public class BlogModel
 	{
+		public long BlogId { get; set; }
+
+		public string BlogTitle { get; set; } = null!;
+
+		public string BlogAuthor { get; set; } = null!;
+
+		public string BlogContent { get; set; } = null!;
+
+		public bool? DeleteFlag { get; set; }
 	}
 }
