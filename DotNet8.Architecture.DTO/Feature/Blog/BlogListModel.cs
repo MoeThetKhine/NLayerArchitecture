8dcf912672fa6bf2d7f10246a39b764319739dfb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 10:18:27 +0630

    Format Code

--- DotNet8.Architecture.DTO/Feature/Blog/BlogListModel.cs
+++ DotNet8.Architecture.DTO/Feature/Blog/BlogListModel.cs
@@ -1,14 +1,13 @@
 ﻿using DotNet8.Architecture.DTO.Feature.PageSetting;
 
-namespace DotNet8.Architecture.DTO.Feature.Blog
+namespace DotNet8.Architecture.DTO.Feature.Blog;
+
+#region BlogListModel
+
+public class BlogListModel
 {
-	#region BlogListModel
-
-	public class BlogListModel
-	{
-		public IEnumerable<BlogModel> DataLst { get; set; }
-		public PageSettingModel PageSetting { get; set; }
-	}
-
-	#endregion
+	public IEnumerable<BlogModel> DataLst { get; set; }
+	public PageSettingModel PageSetting { get; set; }
 }
+
+#endregion
