16df9df3c639aeb01a7c985056e0ac719f5c65f8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-08 10:06:53 +0630

    Format Code

--- DotNet8.Architecture.DTO/Feature/Blog/BlogModel.cs
+++ DotNet8.Architecture.DTO/Feature/Blog/BlogModel.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.Architecture.DTO.Feature.Blog
+﻿namespace DotNet8.Architecture.DTO.Feature.Blog;
+
+public class BlogModel
 {
-	public class BlogModel
-	{
-		public long BlogId { get; set; }
+	public long BlogId { get; set; }
 
-		public string BlogTitle { get; set; } = null!;
+	public string BlogTitle { get; set; } = null!;
 
-		public string BlogAuthor { get; set; } = null!;
+	public string BlogAuthor { get; set; } = null!;
 
-		public string BlogContent { get; set; } = null!;
+	public string BlogContent { get; set; } = null!;
 
-		public bool? DeleteFlag { get; set; }
-	}
+	public bool? DeleteFlag { get; set; }
 }
