1986ea94c0ffaf4cadf1978bd3440fe92f13eb20
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-09 18:41:55 +0630

    -

--- DotNet8.Architecture.BusinessLogic/Features/Blog/BL_Blog.cs
+++ DotNet8.Architecture.BusinessLogic/Features/Blog/BL_Blog.cs
@@ -1,5 +1,22 @@
-﻿namespace DotNet8.Architecture.BusinessLogic.Features.Blog;
+﻿using DotNet8.Architecture.DataAccess.Features.Blog;
+using DotNet8.Architecture.Shared;
+
+namespace DotNet8.Architecture.BusinessLogic.Features.Blog;
 
 public class BL_Blog
 {
+	private readonly DA_Blog _dA_Blog;
+	private readonly BlogValidator _blogValidator;
+
+	public BL_Blog(DA_Blog dA_Blog, BlogValidator blogValidator)
+	{
+		_dA_Blog = dA_Blog;
+		_blogValidator = blogValidator;
+	}
+
+
+
+
+
+
 }
