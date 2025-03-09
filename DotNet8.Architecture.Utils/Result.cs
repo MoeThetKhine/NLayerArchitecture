77968d545afc9fb27e596b7a9f68204f7fb73717
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-09 13:11:21 +0630

    -

--- DotNet8.Architecture.Utils/Result.cs
+++ DotNet8.Architecture.Utils/Result.cs
@@ -2,6 +2,8 @@
 
 namespace DotNet8.Architecture.Utils;
 
+#region Result
+
 public class Result<T>
 {
 	public T Data { get; set; }
@@ -122,3 +124,5 @@ public class Result<T>
 	#endregion
 
 }
+
+#endregion
