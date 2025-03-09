39bad8e73d7287d21f6c99ea8d896d3d5986343e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-09 17:53:26 +0630

    Feat : Add ToModel in Extension

--- DotNet8.Architecture.Extensions/Extension.cs
+++ DotNet8.Architecture.Extensions/Extension.cs
@@ -1,5 +1,18 @@
-﻿namespace DotNet8.Architecture.Extensions;
+﻿using DotNet8.Architecture.DbService.AppDbContextModels;
+using DotNet8.Architecture.DTO.Feature.Blog;
 
-public class Extension
+namespace DotNet8.Architecture.Extensions;
+
+public static class Extension
 {
+	public static BlogModel ToModel(this TblBlog dataModel)
+	{
+		return new BlogModel
+		{
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+		};
+	}
 }
