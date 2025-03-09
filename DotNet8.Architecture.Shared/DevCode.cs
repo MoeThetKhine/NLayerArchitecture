20ae5927a492d97aa952c344f8e7f537a7212d95
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-09 17:18:47 +0630

    Feat : Add   ToJson in DevCode

--- DotNet8.Architecture.Shared/DevCode.cs
+++ DotNet8.Architecture.Shared/DevCode.cs
@@ -1,6 +1,11 @@
-﻿namespace DotNet8.Architecture.Shared
+﻿
+using Newtonsoft.Json;
+
+namespace DotNet8.Architecture.Shared
 {
 	public static class DevCode
 	{
+		public static string ToJson(this object obj) =>
+			JsonConvert.SerializeObject(obj, Formatting.Indented);
 	}
 }
