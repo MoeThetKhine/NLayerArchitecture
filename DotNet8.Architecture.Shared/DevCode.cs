c37c0d77698670600d2635462fbf7c0c63634876
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-09 17:33:58 +0630

    Format Code

--- DotNet8.Architecture.Shared/DevCode.cs
+++ DotNet8.Architecture.Shared/DevCode.cs
@@ -1,26 +1,24 @@
 ﻿using Newtonsoft.Json;
 
-namespace DotNet8.Architecture.Shared
+namespace DotNet8.Architecture.Shared;
+
+#region DevCode
+
+public static class DevCode
 {
+	public static string ToJson(this object obj) =>
+		JsonConvert.SerializeObject(obj, Formatting.Indented);
 
-	#region DevCode
+	public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 
-	public static class DevCode
+	public static bool IsNullOrEmpty(this string str) =>
+   string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+
+	public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
 	{
-		public static string ToJson(this object obj) =>
-			JsonConvert.SerializeObject(obj, Formatting.Indented);
-
-		public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-
-		public static bool IsNullOrEmpty(this string str) =>
-	   string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-
-		public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
-		{
-			return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
-		}
-
+		return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
 	}
 
-	#endregion
 }
+
+#endregion
