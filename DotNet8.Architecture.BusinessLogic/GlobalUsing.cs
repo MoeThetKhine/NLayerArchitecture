d80e5f73f5f6c1c8649794856978cbbb73ef2d62
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-10 13:29:02 +0630

    -

--- DotNet8.Architecture.BusinessLogic/GlobalUsing.cs
+++ DotNet8.Architecture.BusinessLogic/GlobalUsing.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architecture.BusinessLogic
-{
-	internal class GlobalUsing
-	{
-	}
-}
+﻿
