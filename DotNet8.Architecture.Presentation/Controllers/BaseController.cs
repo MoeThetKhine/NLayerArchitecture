bccffb34bb327ed0bea8e56918fdeab18fe09955
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-17 18:55:03 +0630

    -

--- DotNet8.Architecture.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architecture.Presentation/Controllers/BaseController.cs
@@ -6,10 +6,15 @@ namespace DotNet8.Architecture.Presentation.Controllers
 	[ApiController]
 	public class BaseController : ControllerBase
 	{
+
+		#region Content
+
 		protected IActionResult Content(object obj)
 		{
 			return Content(obj.ToJson(), "application/json");
 		}
+
+		#endregion
 
 	}
 }
