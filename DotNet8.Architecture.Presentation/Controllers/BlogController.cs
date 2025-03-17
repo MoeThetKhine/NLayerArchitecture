a4f14d93d8b8921e0e6fca2ec7a1fc11b564dc3f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-17 20:46:24 +0630

    Format Code

--- DotNet8.Architecture.Presentation/Controllers/BlogController.cs
+++ DotNet8.Architecture.Presentation/Controllers/BlogController.cs
@@ -1,40 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Architecture.Presentation.Controllers
+namespace DotNet8.Architecture.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BlogController : BaseController
+	private readonly BL_Blog _bL_Blog;
+
+	public BlogController(BL_Blog bL_Blog)
 	{
-		private readonly BL_Blog _bL_Blog;
-
-		public BlogController(BL_Blog bL_Blog)
-		{
-			_bL_Blog = bL_Blog;
-		}
-
-		#region GetBlogAsync
-
-		[HttpGet]
-		public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
-		{
-			var result = await _bL_Blog.GetBlogAsync(pageNo, pageSize, cancellationToken);
-			return Content(result);
-		}
-
-		#endregion
-
-		#region GetBlogByIdAsync
-
-		[HttpGet("{id}")]
-
-		public async Task<IActionResult> GetBlogByIdAsync(int id, CancellationToken cancellationToken)
-		{
-			var result = await _bL_Blog.GetBlogByIdAsync(id, cancellationToken);
-			return Content(result);
-		}
-
-		#endregion
-
+		_bL_Blog = bL_Blog;
 	}
+
+	#region GetBlogAsync
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		var result = await _bL_Blog.GetBlogAsync(pageNo, pageSize, cancellationToken);
+		return Content(result);
+	}
+
+	#endregion
+
+	#region GetBlogByIdAsync
+
+	[HttpGet("{id}")]
+
+	public async Task<IActionResult> GetBlogByIdAsync(int id, CancellationToken cancellationToken)
+	{
+		var result = await _bL_Blog.GetBlogByIdAsync(id, cancellationToken);
+		return Content(result);
+	}
+
+	#endregion
+
 }
