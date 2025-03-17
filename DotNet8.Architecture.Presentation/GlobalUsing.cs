9381cc47148170a4ca96d1855ca57258a83282b0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-03-17 18:56:07 +0630

    Feat : Add Global Using

--- DotNet8.Architecture.Presentation/GlobalUsing.cs
+++ DotNet8.Architecture.Presentation/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using DotNet8.Architecture.DataAccess.Features.Blog;
 global using DotNet8.Architecture.DbService.AppDbContextModels;
 global using DotNet8.Architecture.Shared;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
